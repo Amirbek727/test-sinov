ca66d4e153094d40754319cab0b63f3ef2ad6d0b
Amirbek727 <amirika738@gmail.com>
2024-10-10 22:43:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+// let son = 3;
+
+//  while (son < 100 ) 
+//    if () {
+
+//    } else {
+      
+//    }
+//     console.log(son); 
+//     son = son + 3;
+
+
+
+// let son = 3;
+
+//  Range (son < 100 ) 
+//    if () {
+
+//    } else {
+      
+//    }
+//     console.log(son); 
+//     son = son + 3;
+// console.log("from 50 to 0")
+// function num(a, b) {
+//   while (a >= b) { 
+//       console.log(a);
+//       a = a - 2; 
+//   }
+// }
+
+// num(50, 0);
+
+  
+
+
+
+
+// console.log("from 50 to 100")
+// function range(a, b) {
+//   while (a <=b ) {
+//     console.log(a);
+//     a = a + 2;
+//   }
+// }
+// range(50, 100);
+
+// let son = 1;
+// do {
+//   console.log(son);s
+//   son++;
+// } while (son <= 10);
+
+for (let son = "#"; son <= "########"; son+="#") {
+  console.log(son);
+}
+
+
---
+++ script.js
@@ -0,0 +1,113 @@
+// let son = 3;
+
+//  while (son < 100 ) 
+//    if () {
+
+//    } else {
+      
+//    }
+//     console.log(son); 
+//     son = son + 3;
+
+
+
+// let son = 3;
+
+//  Range (son < 100 ) 
+//    if () {
+
+//    } else {
+      
+//    }
+//     console.log(son); 
+//     son = son + 3;
+// console.log("from 50 to 0")
+// function num(a, b) {
+//   while (a >= b) { 
+//       console.log(a);
+//       a = a - 2; 
+//   }
+// }
+
+// num(50, 0);
+
+  
+
+
+
+
+console.log("from 50 to 100")
+function range(a, b) {
+  while (a <=b ) {
+    console.log("**" + a + "**");
+    a = a + 2;
+  }
+}
+range(50, 100);
+
+let son = 1;
+do {
+  console.log(son);s
+  son++;
+} while (son <= 10);
+
+
+// function num(a, b){
+// for (let son = 4; son <= 9; son+=1) {
+//     console.log(son);
+//   }
+// }
+
+
+// function range(a, b);
+// let natija = "";
+
+// if (a > b) {
+//   for (let i = a; i >= b; i--) {
+//     natija = natija + i;
+//     console.log(natija);
+//   }
+// } else if (a > b) {
+//   for (let i = a; i <= b; i++) {
+//     natija = natija + i;
+//     console.log(natija);
+//   }
+// }
+
+
+// for (let outer = 1; outer <= 3; outer ++) {
+//   for (let inner = 1; inner <= 4; inner ++) {
+//     console.log(outer + "-etaj", inner + "-xona");
+//   }
+// }
+
+// let myCity = "Olma";
+// console.log(myCity,length);
+// console.log(myCity[3]);
+// console.log(myCity[5]);
+// console.log(myCity[2]);
+// console.log(myCity[4]);
+// console.log(myCity[1]);
+
+// function AppleNum(str) {
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         console.log(str[i]);
+//     } 
+// };
+
+
+// AppleNum("olma");
+// AppleNum("apelsin");
+
+// function teskari(input) {
+//     if (typeof input != "string") input += "";
+//     let natija = "";
+//     for (let i = input.length - 1; i >= 0; i--){
+//         natija += input[i];
+//     }
+//     console.log(natija);
+// } 
+
+// teskari(olma);
+// teskari(madagaskar);
+// teskari(12345);
